c4361a6ec8eac3e9bb0fcd7bd0c2167266903023
RomainMazB <romainmazb@gmail.com>
2021-05-30 00:38:56 +0200

    Make tests pass

--- Plugin.php
+++ Plugin.php
@@ -1,8 +1,12 @@
 <?php namespace SunLab\LevelUp;
 
 use Backend\Facades\Backend;
+use SunLab\LevelUp\Models\ExperienceIncreaser;
+use SunLab\LevelUp\Models\Level;
 use System\Classes\PluginBase;
 use System\Classes\SettingsManager;
+use Winter\Storm\Support\Facades\Event;
+use Winter\User\Models\User;
 
 /**
  * LevelUp Plugin Information File
@@ -36,7 +40,8 @@ class Plugin extends PluginBase
      */
     public function boot()
     {
-
+        $this->extendsUserModel();
+        $this->listenForMeasuresEvents();
     }
 
     /**
@@ -94,5 +99,100 @@ class Plugin extends PluginBase
                 'permissions' => ['sunlab.levelup.manage_levels']
             ]
         ];
+    }
+
+    protected function extendsUserModel()
+    {
+        User::extend(function ($user) {
+            if (!$user->isClassExtendedWith('SunLab.Measures.Behaviors.Measurable')) {
+                $user->extendClassWith('SunLab.Measures.Behaviors.Measurable');
+            }
+
+            $user->belongsToMany['levels'] = [
+                Level::class,
+                'table' => 'sunlab_levelup_levels_users',
+                'pivot' => ['id', 'experience'],
+                'timestamps' => true
+            ];
+
+            $user->addDynamicMethod('getLevelAttribute', function () use ($user) {
+                return $user->levels()
+                        ->orderByDesc('level')
+                        ->first()
+                        ->level ?? 1;
+            });
+
+            $user->addDynamicMethod('getExperienceAttribute', function () use ($user) {
+                return $user->levels()
+                        ->orderByDesc('level')
+                        ->first()
+                        ->pivot
+                        ->experience ?? 0;
+            });
+        });
+    }
+
+    protected function listenForMeasuresEvents()
+    {
+        Event::listen('sunlab.measures.incrementMeasure', static function ($model, $measure, $amount) {
+            if (!$model instanceof User) {
+                return;
+            }
+
+            $experienceIncreaser = ExperienceIncreaser::query()->firstWhere('measure_name', $measure->name);
+
+            if (!$experienceIncreaser) {
+                return;
+            }
+
+            $pointsIncrease = $experienceIncreaser->points * $amount;
+
+            $userLevel =
+                $model->levels()
+                    ->orderByDesc('level')
+                    ->first();
+
+            if ($userLevel) {
+                $userLevel->flushDuplicateCache();
+                $userLevel->pivot->increment('experience', $pointsIncrease);
+                $currentLevel = $userLevel->level;
+                $newExperience = $userLevel->pivot->experience;
+            } else {
+                $firstLevel = Level::query()->firstWhere('level', 1);
+                if ($firstLevel) {
+                    $model->levels()->syncWithoutDetaching([$firstLevel->id => ['experience' => $pointsIncrease]]);
+                    $currentLevel = $firstLevel->level;
+                    $newExperience = $pointsIncrease;
+                }
+            }
+
+            if (!isset($currentLevel)) {
+                return;
+            }
+
+            Event::fire('sunlab.levelup.experienceIncreased', [$model, $newExperience, $pointsIncrease]);
+
+            $nextLevelsReached =
+                Level::query()
+                     ->where([
+                         ['experience_needed', '<=', $newExperience],
+                         ['level', '>', $currentLevel]
+                     ])
+                     ->get();
+
+            if ($nextLevelsReached->isNotEmpty()) {
+                $reachedLevelsRelations = $nextLevelsReached->mapWithKeys(
+                    static function ($level) use ($newExperience) {
+                        return [$level->id => ['experience' => $newExperience]];
+                    }
+                );
+
+                $model->levels()->syncWithoutDetaching($reachedLevelsRelations);
+
+                $nextLevelsReached->sortBy('level')->each(static function ($levelReached) use ($model) {
+                    Event::fire('sunlab.levelup.levelUp', [$model, $levelReached->level]);
+                });
+            }
+        });
     }
 }
